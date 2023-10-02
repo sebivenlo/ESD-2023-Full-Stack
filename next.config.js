11648f44de72a94e0f1768e23d7d6c1ef55e9553
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2023-10-02 20:52:51 +0200

    Code cleanup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Required for hot reloading inside a docker container
+  webpackDevMiddleware:
+    process.env.NODE_ENV !== "production"
+      ? (
+          /** @type {{ watchOptions: { poll: number; aggregateTimeout: number; }; }} */ config
+        ) => {
+          config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+          };
+          return config;
+        }
+      : undefined,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
