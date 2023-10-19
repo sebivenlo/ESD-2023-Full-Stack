2271340d096b82e7eb790adc4c4c83fe3329317b
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2023-10-19 15:09:55 +0200

    Add some useful npm script

--- src/app/examples/trpc/page.tsx
+++ src/app/examples/trpc/page.tsx
@@ -15,7 +15,7 @@ export default async function TRPCExample() {
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 ">
       <h1 className="text-4xl font-bold">{data}</h1>
       <TRPCClientExample />
-      <Link href={`/examples/trpc`}>Go back to main page</Link>
+      <Link href={`/`}>Go back to main page</Link>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Hello World</h1>
       <p className="text-2xl">Argus</p>
       <Link href={`/examples/trpc`}>Go to TRPC example</Link>
+      <Link href={`/todos`}>Go to TODO example</Link>
     </main>
   );
 }
